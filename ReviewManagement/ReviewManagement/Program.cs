ec1c70197aefda2fd9b1c1d2301ed04e4aea0198
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-14 22:35:27 +0530

    [Vaibhav] Add:Create Product Review management

---
+++ ReviewManagement/ReviewManagement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Xml.Linq;
+
+namespace ReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linq query");
+
+            //UC1
+            List<ProductReview> review = new List<ProductReview>()
+            {
+                new ProductReview() {ProductId = 1 , UserID = 1 , Rating = 3.5 , Review = "Fine" , IsLike = true},
+                new ProductReview() {ProductId = 2 , UserID = 2 , Rating = 4.5 , Review = "Good" , IsLike = true},
+                new ProductReview() {ProductId = 7 , UserID = 3 , Rating = 2.5 , Review = "Bad" , IsLike = false},
+                new ProductReview() {ProductId = 4 , UserID = 4 , Rating = 2.1 , Review = "Bad" , IsLike = true},
+                new ProductReview() {ProductId = 5 , UserID = 5 , Rating = 4.8 , Review = "Excellent" , IsLike = true},
+                new ProductReview() {ProductId = 6 , UserID = 6 , Rating = 4.2 , Review = "Good" , IsLike = true},
+                new ProductReview() {ProductId = 7 , UserID = 7 , Rating = 3.8 , Review = "Good" , IsLike = true},
+                new ProductReview() {ProductId = 7 , UserID = 8 , Rating = 1.5 , Review = "Bad" , IsLike = false},
+                new ProductReview() {ProductId = 9 , UserID = 9 , Rating = 4.2 , Review = "Good" , IsLike = true},
+                new ProductReview() {ProductId = 10 , UserID = 10 , Rating = 4.7 , Review = "Excellent" , IsLike = true}
+            };
+            foreach (var data in review)
+            {
+                Console.WriteLine($"ProductId ---> {data.ProductId}  , UserId ---> {data.UserID}  , Rating ---> {data.Rating}  , Review ---> {data.Review}  , IsLike ---> {data.IsLike}");
+            }
+        }
+    }
+}
+
