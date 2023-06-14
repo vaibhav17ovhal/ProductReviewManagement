0e241f806d2223dc227f9fb282509e75b3695a3f
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-14 23:00:19 +0530

    [Vaibhav] Add:Creating Data Table and Adding records

--- ReviewManagement/ReviewManagement/Program.cs
+++ ReviewManagement/ReviewManagement/Program.cs
@@ -51,6 +51,42 @@ namespace ReviewManagement
             Console.WriteLine("\n==========XXX===========\n");
 
             manage.RetrieveProductIDAndReview2(review);
+
+            //UC8
+            // Create a new DataTable
+            DataTable dataTable = new DataTable("ProductReviews");
+
+            // Add columns to the DataTable
+            dataTable.Columns.Add("ProductID", typeof(int));
+            dataTable.Columns.Add("UserID", typeof(int));
+            dataTable.Columns.Add("Rating", typeof(double));
+            dataTable.Columns.Add("Review", typeof(string));
+            dataTable.Columns.Add("IsLike", typeof(bool));
+
+            // Add rows to the DataTable
+            dataTable.Rows.Add(1, 101, 4.5, "Great product!", true);
+            dataTable.Rows.Add(2, 102, 4.1, "Good value for money.", true);
+            dataTable.Rows.Add(1, 103, 2.2, "Not satisfied with the quality.", false);
+            dataTable.Rows.Add(3, 104, 3.3, "Average product!", true);
+            dataTable.Rows.Add(2, 105, 4.2, "Good value for money.", true);
+            dataTable.Rows.Add(1, 106, 3.9, "Good product.", true);
+            dataTable.Rows.Add(1, 107, 1.5, "Bad product!", false);
+            dataTable.Rows.Add(2, 108, 3.5, "Good Product but not worthy.", true);
+            dataTable.Rows.Add(2, 109, 3.0, "Can do better.", false);
+            dataTable.Rows.Add(2, 110, 4.5, "Great product!", true);
+            dataTable.Rows.Add(2, 111, 4.0, "Good value for money.", true);
+            dataTable.Rows.Add(3, 112, 2.3, "Not satisfied with the quality.", false);
+
+            // Display the DataTable contents
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine("ProductID: " + row["ProductID"]);
+                Console.WriteLine("UserID: " + row["UserID"]);
+                Console.WriteLine("Rating: " + row["Rating"]);
+                Console.WriteLine("Review: " + row["Review"]);
+                Console.WriteLine("IsLike: " + row["IsLike"]);
+                Console.WriteLine();
+            }
         }
     }
 }
